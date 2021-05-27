de823b2e945ba90466c4e0c3092244276d54c356
JoseIgnacioTapia <josignaciot@gmail.com>
2021-05-27 15:51:59 -0300

    Cost per Mark

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import { obtenerDiferenciaYear } from '../helper';
+import { obtenerDiferenciaYear, calcularMarca } from '../helper';
 
 const Campo = styled.div`
   display: flex;
@@ -90,6 +90,9 @@ const Formulario = () => {
     // Americano 15%
     // Asiatico 5%
     // Europeo 30%
+
+    resultado = calcularMarca(marca) * resultado;
+    console.log(resultado);
 
     // Basíco aumenta 20%
     // Completo 50%
